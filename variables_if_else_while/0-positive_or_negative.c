e35eaf41324c7e4b8eaffdf70669b5a68efe50e8
nourchaira <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>
2023-10-16 05:05:50 -0700

    Positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("The number %d is ", n);
 	if (n > 0)
 	{
 	printf("positive\n");
 	}
 	else if (n == 0)
- 	{
+	{
 	printf("zero\n");
 	}
 	else
