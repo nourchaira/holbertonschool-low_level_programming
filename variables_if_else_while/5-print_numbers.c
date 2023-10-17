dd577c88d242924f502e75427526fc824fe9ebea
nourchaira <nour.chaira@gmail.com>
2023-10-17 13:35:27 +0100

    digit

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int digit;
+	int digit;
 
-    for (digit = 0; digit <= 9; digit++)
-    {
-        putchar(digit + '0'); /* Convert digit to character and print */
-    }
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0'); /* Convert digit to character and print */
+	}
 
-    putchar('\n'); /* Print a new line */
-    return 0;
+	putchar('\n'); /* Print a new line */
+	return (0);
 }
