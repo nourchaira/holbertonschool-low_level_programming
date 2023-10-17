a0cdbfe24d7636815cd9e5ffa7279dffd5fb5cfd
nourchaira <nour.chaira@gmail.com>
2023-10-17 16:27:09 +0100

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include "main.h" 
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+    printf("_putchar\n");
+    return (0);
 }
