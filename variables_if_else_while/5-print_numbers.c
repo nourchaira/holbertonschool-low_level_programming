fa533b1b66aa19ff91791394df5e39ec8dab075c
nourchaira <nour.chaira@gmail.com>
2023-10-17 13:08:51 +0100

    digit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+int main(void)
+{
+    int digit;
+
+    for (digit = 0; digit <= 9; digit++)
+    {
+        putchar(digit + '0'); // Convert digit to character and print
+    }
+
+    putchar('\n'); // Print a new line
+    return 0;
+}
