ed811aa651d78be90310a3a20d14f6410732a763
nourchaira <nour.chaira@gmail.com>
2023-10-18 13:10:02 +0100

    9 time

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,26 +5,29 @@
  */
 void times_table(void)
 {
-    int row, col, result;
+	int row, col, result;
 
-    for (row = 0; row <= 9; row++)
-    {
-        _putchar('0');  /* Print first element without comma */
-        for (col = 1; col <= 9; col++)
-        {
-            _putchar(',');  /* Print comma before other elements */
-            _putchar(' ');
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0'); /* Print first element without comma */
+		for (col = 1; col <= 9; col++)
+		{
+			_putchar(','); /* Print comma before other elements */
+			_putchar(' ');
 
-            result = row * col;
+			result = row * col;
 
-            if (result < 10) {
-                _putchar(' ');  /* Extra space for single-digit numbers */
-            } else {
-                _putchar(result / 10 + '0');  /* Tens digit */
-            }
+			if (result < 10)
+			{
+				_putchar(' '); /* Extra space for single-digit numbers */
+			}
+			else
+			{
+				_putchar(result / 10 + '0'); /* Tens digit */
+			}
 
-            _putchar(result % 10 + '0');  /* Ones digit */
-        }
-        _putchar('\n');  /* New line after each row */
-    }
+			_putchar(result % 10 + '0'); /* Ones digit */
+		}
+		_putchar('\n'); /* New line after each row */
+	}
 }
