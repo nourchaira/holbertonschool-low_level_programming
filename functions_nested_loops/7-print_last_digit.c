6b03cdf6b079c3a10f4681450f318be018869e4b
nourchaira <nour.chaira@gmail.com>
2023-10-18 12:35:00 +0100

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,12 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit = n % 10;
 
-    if (n < 0)
-        n = -n;
-	else
-    last_digit = n % 10;
-    _putchar(last_digit + '0');
+	if (last_digit < 0)
+		last_digit *= -1;
 
-    return (last_digit);
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
