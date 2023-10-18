8fa65519ba05d912dccb9c116a21add16306af40
nourchaira <nour.chaira@gmail.com>
2023-10-18 10:33:40 +0100

    the absolute value of an integer.

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,17 +1,9 @@
 #include "main.h"
-int absolute_value(int num)
-{
-    if (num < 0) {
-        return -num;
-    } else {
-        return num;
-    }
-}
 
 int main(void)
 {
-    int number = -42; // Replace this with any number you want to find the absolute value of
-    int abs_value = absolute_value(number);
-    printf("Absolute value of %d is: %d\n", number, abs_value);
-    return (0);
+	int number = -42;
+	int abs_value = absolute_value(number);
+	printf("Absolute value of %d is: %d\n", number, abs_value);
+	return 0;
 }
