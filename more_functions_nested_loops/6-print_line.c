300b162407e1ee41165c3fdd5d4888b9f3f8d525
nourchaira <nour.chaira@gmail.com>
2023-10-19 13:53:27 +0100

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,17 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
-		for (i = 0; i < n; i++)
-		{
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        int i;
+        for (i = 0; i < n; i++)
+        {
+            _putchar('_');
+        }
+        _putchar('\n');
+    }
+}
