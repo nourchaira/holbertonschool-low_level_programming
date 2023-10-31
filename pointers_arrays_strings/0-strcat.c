5ab296cf948c78e4251313153ee7f505d3453d35
nourchaira <nour.chaira@gmail.com>
2023-10-31 14:39:31 +0100

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - check the code
+ * @src : char
+ * @dest : char
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int len = strlen(dest);
+
+	while (src[i] != '\0')
+	{
+		dest[len] = src[i];
+		i++;
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
