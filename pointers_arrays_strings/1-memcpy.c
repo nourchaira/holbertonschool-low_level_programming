9de3c66c0dd97760e0bf1f9a00b4dcc0d261c4e0
nourchaira <nour.chaira@gmail.com>
2023-11-02 21:13:32 +0100

    copy memory area

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy -  fills memory with a constant byte
+ * @dest: the address of memory to print
+ * @src: the size of the memory to print
+ * @n: integer
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
