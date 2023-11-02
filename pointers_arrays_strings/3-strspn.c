bbaff7dbacd3a24ca2c85f486968a0c7895d987b
nourchaira <nour.chaira@gmail.com>
2023-11-02 21:15:31 +0100

    length of orefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - check the code
+ * @s : char
+ * @accept : char
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int length = 0;
+    int is_accept[256] = {0};
+
+    while (*accept != '\0')
+    {
+        is_accept[(unsigned char)*accept] = 1;
+        accept++;
+    }
+    while (*s != '\0' && is_accept[(unsigned char)*s])
+    {
+        length++;
+        s++;
+    }
+    return (length);
+}
