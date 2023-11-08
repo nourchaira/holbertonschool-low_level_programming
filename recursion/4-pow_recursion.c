6ecdc13e7709439bd5140cc465e4dea6b401db58
nourchaira <nour.chaira@gmail.com>
2023-11-08 14:00:41 +0100

    returns value

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - check the code
+ * @y :integer
+ * @x :integer
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (_pow_recursion(x, y - 1) * x);
+}
