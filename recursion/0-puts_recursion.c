5afd132b337f2e469562c7b39062d5fd97763aaf
nourchaira <nour.chaira@gmail.com>
2023-11-08 13:24:31 +0100

    print a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Check description
+ * Description: function that checks for uppercase character.
+ * @s: char
+ * Return: void (Success) .
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
