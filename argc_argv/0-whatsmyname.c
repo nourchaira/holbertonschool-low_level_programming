22f8594cd3bd6eea910c8c30899f8c81be552350
nourchaira <nour.chaira@gmail.com>
2023-11-09 15:21:58 +0100

    prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: integer
+ * @argv: char
+ * Return: void (Success)
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
