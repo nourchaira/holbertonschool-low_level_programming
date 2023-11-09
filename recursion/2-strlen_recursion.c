488db7c8f6c39e1be4a9ec6bc9b23a312ee009af
nourchaira <nour.chaira@gmail.com>
2023-11-09 09:18:22 +0100

    return length of a string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * _print_rev_recursion - check the code
- * @s : char
+ * _strlen_recursion - check the code
+ * @s :char
  * Return: Always 0.
  */
 
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return;
+		return (0);
 	}
-	_print_rev_recursion(s + 1);
-	putchar(*s);
+	return (1 + _strlen_recursion(s + 1));
 }
