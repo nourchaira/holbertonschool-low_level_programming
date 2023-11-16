229a051166d060d35a8fd5e9f3001bf3450948b8
nourchaira <nour.chaira@gmail.com>
2023-11-16 14:10:56 +0100

    function that frees dogs.

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * free_dog - check the code
+ * @d : d
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
