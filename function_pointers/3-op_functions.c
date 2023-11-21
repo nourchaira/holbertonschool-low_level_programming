0bcb0e204e75ffa99598dc083bd8d467bf160f36
nourchaira <nour.chaira@gmail.com>
2023-11-21 10:29:54 +0100

    the function that selects the correct function to perform the operation asked by the user.

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+/**
+ * op_add - check the code
+ * @a: int
+ * @b: int
+ * Return: null
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - check the code
+ * @a: int
+ * @b: int
+ * Return: null
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - check the code
+ *
+ * @a: int
+ * @b: int
+ * Return: null
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - check the code
+ *
+ * @a: int
+ * @b: int
+ * Return: null
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - check the code
+ *
+ * @a: int
+ * @b: int
+ * Return: null
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
