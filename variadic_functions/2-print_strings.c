2362020598f359b45e3f3104aa1393f008b58ab2
nourchaira <nour.chaira@gmail.com>
2023-11-23 11:13:09 +0100

    print a string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - check the code
+ * @separator:car
+ * @n: int
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *str;
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+			printf("\n");
+			return;
+		}
+		printf("%s", str);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
