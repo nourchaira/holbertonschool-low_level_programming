b5cf2e239264abd26c31d177b21e581705e2315a
nourchaira <nour.chaira@gmail.com>
2023-12-03 21:46:09 +0100

    prints binary representation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,22 +6,22 @@
  */
 void print_binary(unsigned long int n)
 {
-    int size = sizeof(n) * 8, count = 0;
+	int size = sizeof(n) * 8, count = 0;
 
-    while (size)
-    {
-        if (n & 1L << --size)
-        {
-            putchar('1');
-            count++;
-        }
-        else if (count)
-        {
-            putchar('0');
-        }
-    }
-    if (!count)
-    {
-        putchar('0');
-    }
+	while (size)
+	{
+		if (n & 1L << --size)
+		{
+			putchar('1');
+			count++;
+		}
+		else if (count)
+		{
+			putchar('0');
+		}
+	}
+	if (!count)
+	{
+		putchar('0');
+	}
 }
