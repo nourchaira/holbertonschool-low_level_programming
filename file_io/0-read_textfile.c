83c3401cf68e5471a3b3c413d6392dcef7992370
nourchaira <nour.chaira@gmail.com>
2023-12-10 21:00:31 +0100

    read text file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,9 +8,10 @@
  * Return: the actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
+
 {
 	int file;
-	
+
 	ssize_t bytes;
 
 	char buffer[1024 * 8];
