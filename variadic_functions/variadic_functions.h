7876edeabcd7e66d076206cfabd84a7a84be58be
nourchaira <nour.chaira@gmail.com>
2023-12-12 11:01:47 +0100

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,16 +1,20 @@
-#ifndef VAR
-#define VAR
-#include <stdarg.h>
+#ifndef __VARIADIC_FUNCTIONS__
+#define __VARIADIC_FUNCTIONS__
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
-typedef struct form
+typedef struct types
 {
-	char str;
-	void (*print)(va_list *);
-} fmt;
+    char type;
+    void (*function)( va_list);
+} types_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
+
 #endif
