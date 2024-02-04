504f068036a8ff1a2f4e4b0471f40d39b47c4263
nourchaira <nour.chaira@gmail.com>
2024-02-05 00:13:53 +0100

    search_algos.h

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+/* Standard Libraries */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Functions Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _SORTING_ALGOS_H_ */
