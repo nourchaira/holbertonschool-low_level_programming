478690997269d7d1316573c91736bbcedd08f58d
nanou <nanou@DESKTOP-KPVGBVH>
2024-12-02 06:15:29 +0100

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size : pointer
+ * @action : pointer
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    size_t i;
+
+    if (array && action)
+    {
+        for (i = 0; i < size; i++)
+        {
+            action(array[i]);
+        }
+    }
+}
