a67ffbfcbf1656b08a6f5dc791c8f1c0d6f60090
nanou <nanou@DESKTOP-KPVGBVH>
2024-12-11 16:03:58 +0100

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * swap_int - swaps the values of two integers
  * @a: pointer to the first integer
- * @b: pointer to the second integer
+ * @b: pointer to the second integer.
  */
 void swap_int(int *a, int *b)
 {
