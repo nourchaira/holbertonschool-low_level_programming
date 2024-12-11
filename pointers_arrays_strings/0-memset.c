12be182692db16c216b448158cbc70d8a35c3b75
nanou <nanou@DESKTOP-KPVGBVH>
2024-12-11 15:50:17 +0100

    function that fills memory with a constant

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @s: the address of memory to print
  * @b: the size of the memory to print
  * @n: integer
- * Return: Nothing
+ * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
