1e1a46b539e007921b40bd16c31aa2cec2019dba
nanou <nanou@DESKTOP-KPVGBVH>
2024-12-11 15:58:05 +0100

    copy memory area

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @dest: the address of memory to print
  * @src: the size of the memory to print
  * @n: integer
- * Return: Nothing.
+ * Return: Nothing
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
