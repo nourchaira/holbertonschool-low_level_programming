648e159ada0c70e09d49bab49a75d14014406ae9
nanou <nanou@DESKTOP-KPVGBVH>
2024-12-11 16:26:07 +0100

    searching for set of bytes

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -6,7 +6,7 @@
  * @accept: The bytes to search for
  *
  * Return: A pointer to the byte in s that matches one of the bytes in accept,
- * or NULL if no such byte is found.
+ * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
