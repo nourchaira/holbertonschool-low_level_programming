6d0e0e0a5a0404fb64ac3a05662dbbb390b43839
nanou <nanou@DESKTOP-KPVGBVH>
2024-12-12 21:57:42 +0100

    main.h

---
+++ argc_argv/main.h
@@ -0,0 +1,5 @@
+int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
