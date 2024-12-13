fdc7656b7373431cca8ae6a3421837f2693b9c7e
nanou <nanou@DESKTOP-KPVGBVH>
2024-12-13 14:43:06 +0100

    removedd old work

--- argc_argv/main.h
+++
@@ -1,5 +0,0 @@
-int main(__attribute__((unused)) int argc, char *argv[])
-int main(int argc, __attribute__((unused)) char *argv[])
-int main(int argc, __attribute__((unused)) char *argv[])
-int main(int argc, char *argv[])
-int main(int argc, char *argv[]) 
